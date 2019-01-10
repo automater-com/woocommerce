2b5e6e741f946c2b7dd2ac277befe57fa7352a87
Jakub Kiczko <jakub@kiczko.net>
2019-01-10 19:07:07 +0100

    Version update.

--- automater-pl.php
+++ automater-pl.php
@@ -3,7 +3,7 @@
  * Plugin Name: Automater.pl
  * Plugin URI: https://automater.pl
  * Description: WooCommerce integration with Automater.pl
- * Version: 0.1.7
+ * Version: 0.1.8
  * Author: kutyba.it
  * Author URI: https://kutyba.it
  * Requires at least: 4.8
